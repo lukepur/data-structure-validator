98cc2a2a6938ff5e7eff0ece43bd5503cd866429
Luke Purton <luke.purton@gmail.com>
2017-05-23 16:56:02 +1000

    Start validation procedure

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-const Validator = function constraintConfiguration () {
+const assert = require('assert');
+
+const TARGET_PROP = 'target';
+
+const Validator = function (constraintConfiguration) {
     this.validate = function (data) {
-        return data.a + data.b;
+        // For each constraint member:
+        // 1. Ensure type correct (allow undefined - will be caught by required)
+        // 2. Check for fn: required validation in `validations` (return message if failed)
+        // 3. if children, childValidationMessages = validate(children). If childValidationMessages, return childValidationMessages 
+        // 4. for each remaining validation:
+        //   a) validateMessage = resolveValidation(validation, data)
+        //   b) if validateMessage, return validateMessage
+        // 5. return undefined
+        const validationMessages = [];
+        constraintConfiguration.forEach(constraint => {
+            const targetData = data[constraint.id];
+            
+            // validate type
+            if (!isType(targetData, constraint.type)) {
+                validationMessages.push({
+                    [TARGET_PROP]: constraint.id,
+                    message: `${constraint.id} is not of type ${constraint.type} (got ${typeof targetData}`
+                });
+                return; // One validation message per constraint member
+            }
+
+            // validate required
+        });
+        return validationMessages.length ? validationMessages : undefined;
     };
+
     this.meta = function () {
         return constraintConfiguration;
     };
+}
+
+function isType(data, type) {
+    return typeof data === type || data === undefined;
 }
 
 module.exports = Validator;
@@ -37,6 +69,20 @@ const constraints = [
 
 const validator = new Validator(constraints);
 
-console.log('validate {a: 1, b:1}:', validator.validate({a: 1, b: 1}));
+// Valid
+assert.equal(validator.validate({a: 1, b:1}), undefined);
 
+// Invalid a - not provided
+assert.equal(validator.validate({b: 0}), [{target: 'a', message: 'Argument a is required'}]);
+
+// Invalid a - not a number
+// assert.equal(validator.validate({a: 'string', b: 1}), [{target: 'a', message: 'a must be a number (got "string")'}])
+
+// Invalid b - not provided
+// assert.equal(validator.validate({a: 1}), [{target: 'b', message: 'Argument b is required'}]);
+
+// Invalid b - not a number
+// assert.equal(validator.validate({b: 'string', a: 1}), [{target: 'b', message: 'b must be a number (got "string")'}])
+
+console.log('done');
 process.exit();
